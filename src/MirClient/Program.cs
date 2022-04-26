059fb3873b3d05823b904399c55303681db5ce67
nowme <nowme@outlook.com>
2022-04-26 13:42:44 +0800

    MirClient:嵌入DX9运行文件

--- src/MirClient/Program.cs
+++ src/MirClient/Program.cs
@@ -1,18 +1,45 @@
-﻿
-using System.Text;
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace MirClient
 {
+    static public class NativeMethods
+    {
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr LoadLibrary(string dllToLoad);
+    }
+
+
     internal static class Program
     {
         public static GameFrm Form;
+        const string DxResName = "MirClient.Resources.D3DX9_43.dll";
 
         [STAThread]
         static void Main()
         {
             ApplicationConfiguration.Initialize();
+            LoadUnmanagedLibraryFromResource(Assembly.GetExecutingAssembly(), DxResName, "D3DX9_43.dll");
             Settings.UseTestConfig = true;
             Application.Run(Form = new GameFrm());
+        }
+
+        public static string LoadUnmanagedLibraryFromResource(Assembly assembly, string libraryResourceName,
+            string libraryName)
+        {
+            string tempDllPath = string.Empty;
+
+            using (Stream s = Assembly.GetExecutingAssembly().GetManifestResourceStream(libraryResourceName))
+            {
+                byte[] data = new BinaryReader(s).ReadBytes((int)s.Length);
+
+                string assemblyPath = Path.GetDirectoryName(assembly.Location);
+                tempDllPath = Path.Combine(assemblyPath, libraryName);
+                File.WriteAllBytes(tempDllPath, data);
+            }
+
+            NativeMethods.LoadLibrary(libraryName);
+            return tempDllPath;
         }
     }
 }
